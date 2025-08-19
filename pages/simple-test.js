75334cf511f8901d81727de4993b5a54b61525c2
Dere Samad Babatunde <90105837+deresamad@users.noreply.github.com>
2025-08-19 11:44:20 -0400

    Create simple-test.js file in pages directory

---
+++ pages/simple-test.js
@@ -0,0 +1,8 @@
+export default function SimpleTest() {
+  return (
+    <div>
+      <h1>Simple Test Page</h1>
+      <p>This is a simple test page with no component imports</p>
+    </div>
+  );
+}
